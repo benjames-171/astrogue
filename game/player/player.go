17a1810165cecfdd2b9c583e0c23c7907d14c133
Ben James <benjames171@gmail.com>
2022-01-18 11:26:58 +0000

    update

--- game/player/player.go
+++ game/player/player.go
@@ -79,3 +79,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "bomb-factory"
+  type: "factory"
+  data: "prototype: \"/game/object/bomb.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
