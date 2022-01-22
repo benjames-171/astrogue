df261c485d78d29084cb6171cfeadb5d06b54dcb
Ben James <benjames171@gmail.com>
2022-01-22 11:41:30 +0000

    update

--- game/effects/effect.go
+++ game/effects/effect.go
@@ -13,13 +13,18 @@ components {
     w: 1.0
   }
 }
-components {
-  id: "explode"
-  component: "/game/effects/explode.particlefx"
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/game/core/game.tilesource\"\n"
+  "default_animation: \"empty\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
+    z: 1.0
   }
   rotation {
     x: 0.0
