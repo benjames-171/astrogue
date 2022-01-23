ec100fd38f34b74b9df7259f4b7346b4aec32185
Ben James <benjames171@gmail.com>
2022-01-23 11:20:45 +0000

    update

--- main/data.lua
+++ main/data.lua
@@ -4,10 +4,9 @@ M.STATE_MENU = 1
 M.STATE_CONTROLS = 2
 M.STATE_CREDITS = 3
 M.STATE_PLAYING = 4
-M.STATE_CLEAR = 5
+M.STATE_MINI = 5
 M.STATE_PAUSE = 6
 M.STATE_GAMEOVER = 7
-M.STATE_COMPLETE = 8
 
 M.state = M.STATE_MENU
 
@@ -34,12 +33,6 @@ M.scrap = 0
 M.offset = vmath.vector3()
 M.playerpos = vmath.vector3()
 
-M.savedata = {0,0,0,0, 0,0,0,0}
-
-M.APP_NAME = "app-name"
-M.SAVE_FILE_NAME = "savefile"
-M.SAVE_DATA_SIZE = 8
-
 function M.world2tile(p)
 	return vmath.vector3(math.floor((p.x + M.TILE_SIZE) / M.TILE_SIZE), math.floor((p.y + M.TILE_SIZE) / M.TILE_SIZE), p.z)
 end
@@ -52,31 +45,6 @@ function M.hex2rgba(hex)
 	hex = hex:gsub("#","")
 	local rgba = vmath.vector4(tonumber("0x"..hex:sub(1,2))/255, tonumber("0x"..hex:sub(3,4))/255, tonumber("0x"..hex:sub(5,6))/255, 1)
 	return rgba
-end
-
-function M.onscreen(p, m)
-	if p.x > M.scrollpos.x - m and
-	p.x < M.scrollpos.x + m + M.offset.x * 2 and
-	p.y > M.scrollpos.y - m and
-	p.y < M.scrollpos.y + m + M.offset.y * 2 then
-		return true
-	else
-		return false
-	end
-end
-
-function M.loadgamefile()
-	local file = sys.load(sys.get_save_file(M.APP_NAME, M.SAVE_FILE_NAME))
-
-	if next(file) ~= nil then
-		for n = 1, M.SAVE_DATA_SIZE do
-			M.savedata[n] = file[n]
-		end
-	end
-end
-
-function M.savegamefile()
-	sys.save(sys.get_save_file(M.APP_NAME, M.SAVE_FILE_NAME), M.savedata)
 end
 
 function M.ms2str(time)
